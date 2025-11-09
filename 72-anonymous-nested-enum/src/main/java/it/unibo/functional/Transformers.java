0ad4e66e0280c6495fbcb06c2ec83fde1895710c
Samuele <samuele.rocca2@studio.unibo.it>
2025-11-09 18:03:02 +0100

    add resolution of exercise 72

--- 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/functional/Transformers.java
@@ -54,7 +54,15 @@ public final class Transformers {
      * @return A transformed list where each input element is replaced with the produced elements
      */
     public static <I, O> List<O> transform(final Iterable<I> base, final Function<I, O> transformer) {
-        return null;
+        final Function<I, Collection<O>> transformFunction = new Function<>() {
+
+            @Override
+            public Collection<O> call(final I input) {
+                return List.of(transformer.call(input));
+            }
+            
+        };
+        return flattenTransform(base, transformFunction);
     }
 
     /**
@@ -70,7 +78,8 @@ public final class Transformers {
      * @return A flattened list with the elements of each collection in the input
      */
     public static <I> List<? extends I> flatten(final Iterable<? extends Collection<? extends I>> base) {
-        return null;
+        
+        return flattenTransform(base, Function.identity());
     }
 
     /**
@@ -87,7 +96,19 @@ public final class Transformers {
      * @return A list containing only the elements that passed the test
      */
     public static <I> List<I> select(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        
+        final Function<I, Collection<I>> rejectFuncion = new Function<>() {
+
+            @Override
+            public Collection<I> call(final I input) {
+                if (test.call(input)){
+                    return List.of(input);
+                }
+                return List.of();
+            }
+            
+        };
+        return flattenTransform(base, rejectFuncion);
     }
 
     /**
@@ -103,6 +124,14 @@ public final class Transformers {
      * @return A list containing only the elements that passed the test
      */
     public static <I> List<I> reject(final Iterable<I> base, final Function<I, Boolean> test) {
-        return null;
+        
+        return select(base, new Function<I,Boolean>() {
+
+            @Override
+            public Boolean call(final I input) {
+                return !test.call(input);
+            }
+            
+        });
     }
 }
--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -11,11 +11,82 @@ public final class MonthSorterNested implements MonthSorter {
 
     @Override
     public Comparator<String> sortByDays() {
-        return null;
+        return new SortByDate();
     }
 
     @Override
     public Comparator<String> sortByOrder() {
-        return null;
+        return new SortByMonthOrder();
+    }
+
+    public enum Month{
+        JANUARY("January",31),
+        FEBRUARY("February",28),
+        MARCH("March",31),
+        APRIL("April",30),
+        MAY("May",31),
+        JUNE("June",30),
+        JULY("July",31),
+        AUGUST("August",31),
+        SEPTEMBER("September",30),
+        OCTOBER("October",31),
+        NOVEMBER("November",30),
+        DECEMBER("December",31);
+
+        private final int days;
+        private final String month;
+
+        Month(final String month, final int days) {
+            this.month = month;
+            this.days = days;
+        }
+
+        public String getMonth(){
+            return this.month;
+        }
+
+        static Month fromString(final String month){
+            Month monthfromString = null;
+            String upperCaseMonth = month.toUpperCase();
+            for (final Month i : Month.values()) {
+                if (i.getMonth().toUpperCase().startsWith(upperCaseMonth)){
+                    if (monthfromString != null){
+                        throw new IllegalArgumentException("Exist some month that stats with " + upperCaseMonth);
+                    }else{
+                        monthfromString = i;
+                    }
+                }
+            }
+            if (monthfromString == null){
+                throw new IllegalArgumentException("The month name does not exist");
+            }
+            return monthfromString;
+        }
+        
+    }
+
+    private final class SortByMonthOrder implements Comparator<String>{
+
+        @Override
+        public int compare(String o1, String o2) {
+            final Month month1 = Month.fromString(o1);
+            final Month month2 = Month.fromString(o2);
+
+            return Integer.compare(month1.ordinal(), month2.ordinal());
+        }
+
+    }
+    
+    private class SortByDate implements Comparator<String>{
+
+        @Override
+        public int compare(String o1, String o2) {
+            
+            final Month month1 = Month.fromString(o1);
+            final Month month2 = Month.fromString(o2);
+        
+            return Integer.compare(month1.days, month2.days);
+        }
+
     }
 }
